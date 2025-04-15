1227c3593e0f09d5f7038449e11946ab8a0275f9
vamsi duppala <vamsiduppala@gmail.com>
2025-04-15 02:18:59 -0400

    initial commit

---
+++ src/main/java/com/nearbybank/find/NearbybankApplication.java
@@ -0,0 +1,15 @@
+package com.nearbybank.find;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbybankApplication.class, args);
+		
+		System.out.println("Fff");
+	}
+
+}
---
+++ src/test/java/com/nearbybank/find/NearbybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nearbybank.find;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
